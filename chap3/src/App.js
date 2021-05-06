45114b8c9f7716d87fc3609d2d5e666e9f72d7c2
Yudon Lee <future0159@naver.com>
2021-05-07 02:18:33 +0900

    chap3 start! at least one small chapter everyday

---
+++ chap3/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+// chap3.1 Classtype component
+class App extends Component {
+  render() {
+    const name = 'react'
+    return <div className="react">{name}</div>;
+  }
+}
+export default App;
