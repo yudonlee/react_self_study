ef662028891e867a99c47ea1d5b33459050c565c
Yudon Lee <future0159@naver.com>
2021-05-08 02:37:43 +0900

    chap3.2 module import and export

--- chap3/src/App.js
+++ chap3/src/App.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+// chap 3.2 module export and import
+// import React from 'react';
+// import MyComponent from './MyComponent';
 
-// chap3.1 Classtype component
-class App extends Component {
-  render() {
-    const name = 'react'
-    return <div className="react">{name}</div>;
-  }
-}
-export default App;
+// const App = () => {
+//   return <MyComponent />;
+// };
+
+// export default App;
+
+// class App extends Component {
+// import React, { Component } from 'react'
+
+// // chap3.1 Classtype component
+//   render() {
+//     const name = 'react'
+//     return <div className="react">{name}</div>;
+//   }
+// }
+// export default App;
---
+++ chap3/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// chap 3.2.3 module import and export (if using rsc ,more easier to make!)
+const MyComponent = () => {
+    return (
+        <div>
+        my cool and new component
+        </div>
+    );
+};
+
+export default MyComponent;
