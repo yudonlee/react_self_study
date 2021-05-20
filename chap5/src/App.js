9b793a5eaf06170436ee7cde1d40f69c421eae8f
Yudon Lee <future0159@naver.com>
2021-05-21 01:51:37 +0900

    chapter5 start!

---
+++ chap5/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import ValidationSample from './ValidationSample';
+class App extends Component {
+  render() {
+    return (
+      <ValidationSample/>
+    );
+  }
+}
+export default App;
