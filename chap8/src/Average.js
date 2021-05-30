4d9b1c0823d4fc99b8c9f82f3adb8796af4fb622
Yudon Lee <future0159@naver.com>
2021-05-31 01:52:42 +0900

    8.5 finished

--- chap8/src/Average.js
+++ chap8/src/Average.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 const getAverage = numbers => {
-    console.log('평균값 계산중');
+    console.log('평균값 계산 중..');
     if (numbers.length === 0) return 0;
     const sum = numbers.reduce((a,b) => a + b);
     return sum / numbers.length;
@@ -10,29 +10,110 @@ const getAverage = numbers => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
-
-    const onChange = e => {
+    
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
-    const onInsert = e => {
+    }, []);
+    const onInsert = useCallback(() => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
-    
+    }, [number, list]);
+
+    const avg = useMemo(() => getAverage(list), [list]);
     return (
         <div>
-            <input value={number} onChange = {onChange} />
-            <button onClick = {onInsert}>등록</button>
+            <input value={number} onChange={onChange} />
+            <button onClick={onInsert}>등록</button>
             <ul>
-                {list.map((value,index) => (
+                {list.map((value, index) => (
                     <li key={index}>{value}</li>
                 ))}
             </ul>
             <div>
-                <b>평균값</b> {getAverage(list)}
+                <b>평균값</b> {avg}
             </div>
         </div>
-    )
-}
+    )   
+};
+
 export default Average;
+
+// import React, { useState, useMemo } from 'react';
+
+// const getAverage = numbers => {
+//     console.log('평균값 계산 중..');
+//     if (numbers.length === 0) return 0;
+//     const sum = numbers.reduce((a, b) => a + b);
+//     ruturn sum / numbers.length;
+// };
+
+// const Average = () => {
+//     const [list, setList] = useState([]);
+//     const [list, setNumber] = useState('');
+
+//     const onChange = e => {
+//         setNumber(e.target.value);
+//     };
+//     const onInsert = () => {
+//         const nextList = list.concat(parseInt(number));
+//         setList(nextList);
+//         setNumber('');
+//     };
+//     const avg = useMemo(() => getAverage(list), [list]);
+
+//     return (
+//         <div>
+//             <input value={number} onChange={onChange} />
+//             <button onClick={onInsert}>등록</button>
+//             <ul>
+//                 {list.map((value, index) => (
+//                     <li key={index}>{value}</li>
+//                 ))}
+//             </ul>
+//             <div>
+//                 <b>평균값:</b> {avg}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Average;
+// import React, { useState } from 'react';
+
+// const getAverage = numbers => {
+//     console.log('평균값 계산중');
+//     if (numbers.length === 0) return 0;
+//     const sum = numbers.reduce((a,b) => a + b);
+//     return sum / numbers.length;
+// };
+
+// const Average = () => {
+//     const [list, setList] = useState([]);
+//     const [number, setNumber] = useState('');
+
+//     const onChange = e => {
+//         setNumber(e.target.value);
+//     };
+//     const onInsert = e => {
+//         const nextList = list.concat(parseInt(number));
+//         setList(nextList);
+//         setNumber('');
+//     };
+    
+//     return (
+//         <div>
+//             <input value={number} onChange = {onChange} />
+//             <button onClick = {onInsert}>등록</button>
+//             <ul>
+//                 {list.map((value,index) => (
+//                     <li key={index}>{value}</li>
+//                 ))}
+//             </ul>
+//             <div>
+//                 <b>평균값</b> {getAverage(list)}
+//             </div>
+//         </div>
+//     )
+// }
+// export default Average;
