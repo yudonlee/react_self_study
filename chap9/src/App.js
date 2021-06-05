fe8afe7205b5a69058c8f2da1c63f2a3c8dc2505
Yudon Lee <future0159@naver.com>
2021-06-05 23:56:48 +0900

    chapter 9.2 start

--- chap9/src/App.js
+++ chap9/src/App.js
@@ -1,27 +1,54 @@
-import React, { Componenet } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Componenet {
+import React, { Component } from 'react';
+import SassComponent from './SassComponent';
+ 
+class App extends Component {
   render() {
     return (
-      <div ClassName="App">
-        <header>
-          <img src={logo} className="logo" alt="logo" />
-          <p>
-            Edit <codee>src/App.js</codee> and save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-        </header>
+      <div>
+        <SassComponent />
       </div>
-    )
+    );
   }
 }
-
+ 
 export default App;
+
+// import React, { Componenet } from 'react';
+// import SassComponent from './SassComponent';
+// import './SassComponent.scss';
+// class App extends Componenet {
+//   render() {
+//     return (
+//       <div>
+//         <SassComponent />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+  
+// class App extends Componenet {
+//   render() {
+//     return (
+//       <div ClassName="App">
+//         <header>
+//           <img src={logo} className="logo" alt="logo" />
+//           <p>
+//             Edit <codee>src/App.js</codee> and save to reload.
+//           </p>
+//           <a
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             >
+//               Learn React
+//             </a>
+//         </header>
+//       </div>
+//     )
+//   }
+// }
+
---
+++ chap9/src/SassComponent.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SassComponent.scss';
+
+
+const SassComponent = () => {
+  return (
+    <div className="SassComponent">
+      <div className="box red" />
+      <div className="box orange" />
+      <div className="box yellow" />
+      <div className="box green" />
+      <div className="box blue" />
+      <div className="box indigo" />
+      <div className="box violet" />
+    </div>
+  );
+};
+export default SassComponent;
+
+
+// import React from 'react';
+// import './SassComponet.scss'
+
+// const SassComponent = () =>{
+//     return (
+//         <div className="SassCompoent">
+//             <div className="box red" />
+//             <div className="box orange" />
+//             <div className="box yellow" />
+//             <div className="box green" />
+//             <div className="box blue" />
+//             <div className="box indigo" />
+//             <div className="box violet" />    
+//         </div>
+//     )
+// }
+// export default SassComponent
