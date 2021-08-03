c7fd6426327a7fe8cc07fbe6503a10970f4a4027
Yudon Lee <future0159@naver.com>
2021-08-04 01:07:23 +0900

    9.3 end

---
+++ chap9/src/CSSModule.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './CSSModule.module.css'
+const CSSModule = () => {
+  return (
+      <div className={styles.wrapper}>
+          안녕하세요, 저는 <span className="something">CSS module!</span>
+      </div>
+  )  
+};
+
+export default CSSModule;
