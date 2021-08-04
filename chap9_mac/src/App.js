eeb957208b52d8b89732188f92c788d339aabfa3
yudonlee <future0159@hanyang.ac.kr>
2021-08-04 14:03:27 +0900

    chap9 finished

---
+++ chap9_mac/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import  StyledComponent from './StyledComponent';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
+export default App;
