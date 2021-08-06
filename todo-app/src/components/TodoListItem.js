6e7a65119a14c27827bb4ad7bfa2f77e63c79c30
yudonlee <future0159@hanyang.ac.kr>
2021-08-06 11:19:58 +0900

    implement remove figure

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 import {
-    MdCheckBoxOutlineBlank,
-    MdCheckBox,
-    MdRemoveCircleOutline,
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
 } from 'react-icons/md';
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo }) => {
-    const { text, checked } = todo;
-    return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox', {checked})}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove">
-                <MdRemoveCircleOutline />
-            </div>
-        </div>
-    );
+const TodoListItem = ({ todo, onRemove }) => {
+  const { id, text, checked } = todo;
+  return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
 };
 
 export default TodoListItem;
